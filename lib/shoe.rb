2a37fabf075c9e37bb148fa41f3e23632b1b6f2b
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-29 22:03:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,9 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include?(@brand))
+   # if !(BRANDS.include?(@brand))
       BRANDS << @brand
-    end
+   # end
   end
   # def brand=(brand)
   # @brand = brand
